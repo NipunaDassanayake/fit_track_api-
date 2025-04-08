630b5f0747516e76130a6c68848bb03f63a2563c
NipunaDassanayake <nipuna920@gmail.com>
2025-04-09 01:18:34 +0530

    "created all the Repositories"

---
+++ src/main/java/com/fit_track_api/fit_track_api/repository/WorkoutPlanRepository.java
@@ -0,0 +1,13 @@
+package com.fit_track_api.fit_track_api.repository;
+import com.fit_track_api.fit_track_api.model.User;
+import com.fit_track_api.fit_track_api.model.WorkoutPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlan, Long> {
+    List<WorkoutPlan> findByUser(User user);
+    List<WorkoutPlan> findByIsPublicTrueOrderByIdDesc();
+    List<WorkoutPlan> findByUserInAndIsPublicTrueOrderByIdDesc(List<User> users);
+}
